7f006a2f7cf7f2f85e188217c0f205513f006191
Satadru Bhattacharjee <Satadru.Bhattacharjee@intigral.net>
2019-07-24 12:00:03 +0400

    Spatial Navigation for WebOS and Tizen

--- packages/intigral-template-jawwy-tv/index.tizen.js
+++ packages/intigral-template-jawwy-tv/index.tizen.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import { Api } from 'renative';
 import { TIZEN, FORM_FACTOR_TV, PLATFORM_GROUP_SMARTTV, registerServiceWorker } from 'renative';
 import App from './src/app';
-import spatialFocus from './src/utils/focus';
+import { initNavigation } from '@noriginmedia/react-spatial-navigation';
 
 Api.platform = TIZEN;
 Api.formFactor = FORM_FACTOR_TV;
 Api.platformGroup = PLATFORM_GROUP_SMARTTV;
-spatialFocus();
+
+initNavigation({
+    debug: true,
+    visualDebug: false
+});
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
--- packages/intigral-template-jawwy-tv/index.webos.js
+++ packages/intigral-template-jawwy-tv/index.webos.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { Api } from 'renative';
 import { WEBOS, FORM_FACTOR_TV, PLATFORM_GROUP_SMARTTV, registerServiceWorker } from 'renative';
 import App from './src/app';
-import spatialFocus from './src/utils/focus';
+import { initNavigation } from '@noriginmedia/react-spatial-navigation';
 
 Api.platform = WEBOS;
 Api.formFactor = FORM_FACTOR_TV;
 Api.platformGroup = PLATFORM_GROUP_SMARTTV;
-spatialFocus();
+
+initNavigation({
+    debug: true,
+    visualDebug: false
+});
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
